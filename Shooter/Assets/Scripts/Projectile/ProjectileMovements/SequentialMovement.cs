e08948110e5841a8f0e9e92a93e2176461e2d92a
Joshua Krauchi <jkrauchi@my.bcit.ca>
2022-06-05 10:00:24 -0700

    Did a bit more work on SequentialMovement.

--- Shooter/Assets/Scripts/Projectile/ProjectileMovements/SequentialMovement.cs
+++ Shooter/Assets/Scripts/Projectile/ProjectileMovements/SequentialMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 /**
@@ -21,17 +22,17 @@ public class SequentialMovement : ProjectileMovement
     {
         base.Awake();
 
-        /*ProjectileMovements = GetComponentsInChildren<ProjectileMovement>();
+        ProjectileMovements = GetComponents<ProjectileMovement>().Skip(1).ToArray();
 
         if (ProjectileMovements.Length <= 0)
         {
-            throw new Exception("A projectile with the SequentialMovement component must have a child GameObject attached with ProjectileMovements to run.");
+            throw new Exception("A projectile with the SequentialMovement component must have other ProjectileMovements to run.");
         }
 
         if (StartTimes.Length != ProjectileMovements.Length - 1)
         {
-            throw new Exception("Number of StartTimes must be 1 less than the number of ProjectileMovements in the child.");
-        }*/
+            throw new Exception("Number of StartTimes must be 1 less than the number of ProjectileMovements.");
+        }
     }
 
     public override void UpdateTransform(bool isRewinding)
